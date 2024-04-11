2f19c22edf5882794895be43c4b29f6bef076213
Deepthi <deeptijnair@gmail.com>
2024-04-11 07:51:32 -0400

    Added some accessibility and css changes

--- podcast.js
+++ podcast.js
@@ -1,15 +1,15 @@
-var emailInput = document.getElementById('email-input');
-var emailError = document.getElementById('email-error');
+    var emailInput = document.getElementById('email-input');
+    var emailError = document.getElementById('email-error');
 
 
-function validateEmail(){
+    function validateEmail(){
 
-    if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML="Oops! Please check your email";
-        content.style.color = 'red';
-        return false;
+        if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+            emailError.innerHTML="Oops! Please check your email";
+            content.style.color = 'red';
+            return false;
+        }
+        emailError.innerHTML = "";
+        return true;
     }
-    emailError.innerHTML = "";
-    return true;
-}
 
